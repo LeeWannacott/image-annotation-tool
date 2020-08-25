ba3d0d464fe940dec3309d29f6ae384a9c94aa73
LeeWannacott <wannacottL@gmail.com>
2020-08-26 02:27:24 +1200

    calculates bounding boxing coordinates

--- image_selector_from_video.py
+++ image_selector_from_video.py
@@ -21,7 +21,7 @@ Space bar = Go to next set of images and store tagged images + frame number in t
 # variables that can be change by the user:
 
 # Select file path to read video file from.
-cap = cv2.VideoCapture('C:\\Users\\Lee\\Desktop\\image-selector-opencv-python\\kurt.mp4')
+cap = cv2.VideoCapture('C:\\Users\\Lee\\Desktop\\image-selector-opencv-python\\video.mp4')
 
 # Number of rows is changeable value. Default value is 7.
 number_of_rows = 7
@@ -31,7 +31,7 @@ window_width = 1800
 window_height = 1050
 
 # Original images from video that will be resized down to fit into grid.
-initial_img_width = 1980
+initial_img_width = 1920
 initial_img_height = 1080
 
 # ************************************************************************
@@ -93,8 +93,6 @@ bounding_box_start_coordinates_x_y = ()
 last_mouse_button_clicked = []
 drawn_one_cell_or_span = []
 
-
-
 # Mouse click for left button
 def click_event(event, x, y, flags, param):
     global animal
@@ -103,7 +101,6 @@ def click_event(event, x, y, flags, param):
     global new_image
     global image_list_for_bounding_boxes
     global drawn_one_cell_or_span
-
 
     if event == cv2.EVENT_LBUTTONDOWN and animal == '' and enable_draw_on_grid == True:
         last_mouse_button_clicked.append('left')
@@ -134,10 +131,13 @@ def click_event(event, x, y, flags, param):
             # https://stackoverflow.com/questions/8669189/converting-numbers-within-grid-to-their-corresponding-x-y-coordinates
             x2 = cell_number % number_of_columns
             y2 = cell_number // number_of_columns
+
             cell_x_position = x2 * cell_width
             cell_y_position = y2 * cell_height
+
             draw_to_x = cell_x_position + cell_width
             draw_to_y = cell_y_position + cell_height
+            # print(x2,y2,cell_x_position,cell_y_position,draw_to_x,draw_to_y)
             cv2.rectangle(param[0], (int(cell_x_position), int(cell_y_position)), (int(draw_to_x), int(draw_to_y)),
                           (0, 150, 0), 2)
             cv2.imshow('image_selector_from_video', param[0])
@@ -174,7 +174,7 @@ def click_event(event, x, y, flags, param):
                 if animal == '':
                     # window_open = True
                     animal = 'window open'
-                    animal = easygui.enterbox("What animal is it?")
+                    animal = easygui.enterbox("What is tagged in the images?")
 
                 if animal != 'window open':
                     if animal is None:
@@ -229,46 +229,72 @@ def click_event(event, x, y, flags, param):
             image_list.pop()
             last_mouse_button_clicked.pop()
 
-
-
-
-    # Allow bounding boxes to be places over images.
+    # Allow bounding boxes to be places over images
     elif event == cv2.EVENT_MBUTTONDOWN:
 
         def get_bounding_box_start_coordinates(x, y):
             x_start_boundary = x
             y_start_boundary = y
+            # Calculates starting cell number
             col_number = x / cell_width
             x1 = math.trunc(col_number)
             row_number = y / cell_height
             y1 = math.trunc(row_number)
             cell_number_on_start_of_drawing = int(x1 + (y1 * number_of_columns))
 
-            return (x_start_boundary, y_start_boundary, cell_number_on_start_of_drawing)
+            return x_start_boundary, y_start_boundary, cell_number_on_start_of_drawing
 
         bounding_box_start_coordinates_x_y = get_bounding_box_start_coordinates(x, y)
 
     elif event == cv2.EVENT_MBUTTONUP:
-        # Get cell number at start of
+        # Get cell number at end of bounding box
         col_number = x / cell_width
-        x1 = math.trunc(col_number)
+        cell_x = math.trunc(col_number)
         row_number = y / cell_height
-        y1 = math.trunc(row_number)
-        cell_number_on_end_of_drawing = int(x1 + (y1 * number_of_columns))
+        cell_y = math.trunc(row_number)
+        cell_number_on_end_of_drawing = int(cell_x + (cell_y * number_of_columns))
+        # Minus x,y positions to get cells relative position
+        cell_x_position = cell_x * cell_width
+        cell_y_position = cell_y * cell_height
+
+        # print(cell_x_position, cell_y_position)
+        # print(bounding_box_start_coordinates_x_y[0],bounding_box_start_coordinates_x_y[1])
 
         def draw_boundary_box(x, y, start_boundary_x_and_y):
             # Making copy for un-drawing bounding box
             new_image_boundary = param[0].copy()
             image_list.append(new_image_boundary)
 
+            # Draws bounding box
             end_boundary_x_and_y = (x, y)
             cv2.rectangle(param[0], (start_boundary_x_and_y[0], start_boundary_x_and_y[1]),
                           (end_boundary_x_and_y[0], end_boundary_x_and_y[1]), (0, 150, 150), 2)
             cv2.imshow('image_selector_from_video', param[0])
+
+            # Used for un-drawing bounding box logic
             last_mouse_button_clicked.append('middle')
 
+            # Makes the coordinates relative to within that cell rather than the whole window.
+            cell_start_relative_position_x = start_boundary_x_and_y[0] - cell_x_position
+            cell_start_relative_position_y = start_boundary_x_and_y[1] - cell_y_position
+            cell_end_relative_position_x = end_boundary_x_and_y[0] - cell_x_position
+            cell_end_relative_position_y = end_boundary_x_and_y[1] - cell_y_position
 
-        # Allows drawing of boundary boxes if its still within the sane cell
+            # Resizing coordinates back to the those of the original sized images rounded to whole pixels.
+            cell_start_relative_position_x_resized = round(cell_start_relative_position_x / resize_x)
+            cell_start_relative_position_y_resized = round(cell_start_relative_position_y / resize_y)
+            cell_end_relative_position_x_resized = round(cell_end_relative_position_x / resize_x)
+            cell_end_relative_position_y_resized = round(cell_end_relative_position_y / resize_y)
+
+            print(cell_start_relative_position_x_resized, cell_start_relative_position_y_resized,
+                  cell_end_relative_position_x_resized, cell_end_relative_position_y_resized)
+
+
+
+
+
+
+        # Checks if its still within the same cell and if so draws bounding box
         if bounding_box_start_coordinates_x_y[2] == cell_number_on_end_of_drawing:
             draw_boundary_box(x, y, bounding_box_start_coordinates_x_y)
 
@@ -366,21 +392,21 @@ def image_grid(index, x_offset=0, y_offset=0, i=0):
 
                             # Calculates frames spans backwards and forwards
                             def make_list_of_frames_to_keep(animal_count=0):
-                                # putting into a list of two's for calculating frame spans
+                                # Putting into a list of two's for calculating frame spans
                                 frame_numbers_list_sliced = zip(frame_numbers_list[0::2], frame_numbers_list[1::2])
-                                # clear lists when space pressed
+                                # Clear lists when space pressed
                                 list_of_frames_to_keep.clear()
                                 animal_list_to_print.clear()
 
                                 for numbers in frame_numbers_list_sliced:
-                                    # forward frame spans
+                                    # Forward frame spans
                                     if numbers[0] < numbers[1]:
                                         for num1 in range(numbers[0], numbers[1] + 1):
                                             list_of_frames_to_keep.append(num1)
                                             animal_list_to_print.append(animal_list_to_keep[animal_count])
                                         animal_count += 1
 
-                                    # backward frame spans
+                                    # Backward frame spans
                                     elif numbers[0] > numbers[1]:
                                         for num2 in range(numbers[1], numbers[0] + 1):
                                             list_of_frames_to_keep.append(num2)
@@ -389,11 +415,11 @@ def image_grid(index, x_offset=0, y_offset=0, i=0):
 
                             make_list_of_frames_to_keep()
 
-                            # clearing temporary lists, so ready for the next lot of images.
+                            # Clearing temporary lists, so ready for the next lot of images.
                             cell_numbers_list_for_each_grid.clear()
                             animal_list_temporary.clear()
 
-                            # creating text file with frame numbers and what user tags images as.
+                            # Creating text file with frame numbers and what user tags images as.
                             file = open('List_of_images.txt', 'w')
                             for i, frame in enumerate(list_of_frames_to_keep):
                                 file.write(str(frame) + str(' ' + animal_list_to_print[i]) + '\n')
